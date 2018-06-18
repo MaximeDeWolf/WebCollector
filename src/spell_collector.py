a0797ec84024f2ca22ebd155e264c8a4e260a017
MaximeDeWolf <MaximeDeWolf@outlook.fr>
2018-06-18 20:14:06 +0200

    Adding some documentation

--- src/spell_collector.py
+++ src/spell_collector.py
@@ -2,12 +2,24 @@ import sys
 import web_collector as WC
 import lxml.etree as ET
 
+"""
+This file is an example of how to use this library
+"""
+
 def excludeNonBasicSpells(XPathObject):
+    """
+    Analyse an XPath element and return true iff it matches a spell which make
+    parts of the basic rule book of Pathfinder.
+    """
     root = ET.HTML(XPathObject)
     icons = root.xpath('//img[@class="opachover"]')
     return len(icons)==0
 
 def makeShallow(list_):
+    """
+    Browse a list and roll up all its items in order to place them at the same depth.
+    For example, the list [a, [b, [c, d, e]], [[[f]]] ] will become [a, b, c, d, e, f].
+    """
     shallowList = []
     for elem in list_:
         if isinstance(elem, list):
@@ -17,19 +29,29 @@ def makeShallow(list_):
     return shallowList
 
 def cleanData(data):
-    if len(data) == 1 :
-        newData = data[0]
-        return newData.replace('\t', '').replace('\n', '').replace('\r', '')
-    else:
-        newData = []
-        for i, string in enumerate(data):
-            newData[i] = string.replace('\t', '').replace('\n', '').replace('\r', '')
-        return newData
+    """
+    Remowe all the special from the 'data'
+    """
+    newData = []
+    for string in data:
+        newData.append(string.replace('\t', '').replace('\n', '').replace('\r', ''))
+    return newData
 
 #'//div[@id="PageContentDiv"]'
 
-pattern = {
-    "spell" : '//h1[@class="pagetitle"]/text()'
+spellPattern = {
+    "name" : '//h1[@class="pagetitle"]/text()',
+    "school" : '//div[@id="PageContentDiv"]//b[1]/following-sibling::a[1]/text()',
+    "class" : '//div[@id="PageContentDiv"]/child::node()[following::node()[text()="Temps d\'incantation"][preceding::node()[text()="Niveau"]]/text()',
+    #"level" : '//div[@id="PageContentDiv"]//b[2]/following-sibling::text()[position() < count(//div[@id="PageContentDiv"]//b[3]/preceding-sibling::text())-1]',
+    "time" : '//h1[@class="pagetitle"]/text()',
+    "component" : '//h1[@class="pagetitle"]/text()',
+    "range": '//h1[@class="pagetitle"]/text()',
+    "target": '//h1[@class="pagetitle"]/text()',
+    "duration": '//h1[@class="pagetitle"]/text()',
+    "JS": '//h1[@class="pagetitle"]/text()',
+    "RM": '//h1[@class="pagetitle"]/text()',
+    "effect": '//h1[@class="pagetitle"]/text()'
 }
 
 if __name__ == '__main__':
@@ -43,5 +65,11 @@ if __name__ == '__main__':
     spellsInfo = web_collector.fetchPage(spellsURL[0:10])
     #spellsInfo = makeShallow(spellsInfo)
     basicSpells = list(filter(excludeNonBasicSpells, spellsInfo))
-    spellName = web_collector.extract(basicSpells, pattern, strTransformer=cleanData)
-    print(spellName)
+    spellName = web_collector.extract(basicSpells, spellPattern, strTransformer=cleanData)
+    #spellName = web_collector.extract(basicSpells, spellPattern)
+    root = ET.HTML(basicSpells[0])
+    res = root.xpath('//div[@id="PageContentDiv"]/b[3]/preceding-sibling::node()')
+    print(res)
+    #test = ET.tostring(res, pretty_print=True)
+    #print(test)
+    print(spellName[0]["class"])
--- src/web_collector.py
+++ src/web_collector.py
@@ -9,6 +9,10 @@ class webCollector:
         self._http = urllib3.PoolManager()
 
     def listToSingle(function):
+        """
+        This function helps to threat a list as it was a single object.
+        Designed to be used as a decorator.
+        """
         def listHandler(self, url, *args, **kwargs):
             if isinstance(url, list):
                 res = []
@@ -20,6 +24,9 @@ class webCollector:
         return listHandler
 
     def strToHTML(function):
+        """
+        Ensure that a certain parameter is always an Xpath object
+        """
         def converter(self, etree, *args, **kwargs):
             if isinstance(etree, ET._Element):
                 return function(self, etree, *args, **kwargs)
@@ -30,16 +37,26 @@ class webCollector:
 
     @listToSingle
     def fetchPage(self, url):
+        """
+        Get the html code store at the 'url' address
+        """
         response = self._http.request('GET', url)
         return response.data.decode('utf-8')
 
     def fetchAndExtract(self, url, XPathExpression):
+        """
+        Get the html code from 'url' and extract an Xpath object obtained
+        thanks to 'XPathExpression'
+        """
         page = self.fetchPage(url)
         data = self.extractData(page, XPathExpression)
         return data
 
     @listToSingle
     def extractData(self, page, XPathExpression):
+        """
+        Return the result of an 'XPathExpression' over a 'page'
+        """
         root = ET.HTML(page)
         data = root.xpath(XPathExpression)
         return data
@@ -47,6 +64,16 @@ class webCollector:
     @listToSingle
     @strToHTML
     def extract(self, etree, pattern, strTransformer=lambda x:x):
+        """
+        Browse a dictionnary and replace all the XPath expression it contains
+        by the result it produces on the 'etree' Xpath object.
+
+        For example:
+        pattern = {'name': '//name/text()'},
+        etree = <name>John Doe<\>
+
+        will produce this result {'name': 'John Doe'}
+        """
         data = {}
         for key in pattern.keys():
             if isinstance(pattern[key], dict) or isinstance(pattern[key], list):
